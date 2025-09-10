7409ec6581f501c52d41b91e6b7ae02255593c30
Aidan <bantezea@gmail.com>
2025-09-10 10:46:12 +0300

    Introduction to chapter

--- python/chapterOne.py
+++ python/chapterOne.py
@@ -5,3 +5,4 @@ print('Cool!', name, 'I like', song, 'too')
 
 print('Today' , 'is' , 'Tuesday' , 'wow' , sep='!?', end='!#$%@&' )
 print('The selected song has', len(song), 'words')
+
---
+++ python/chapterTwo.py
@@ -0,0 +1,6 @@
+# Write a program that reads in two strings, str1 and str2, and an integer, count. Concatenate the two
+# strings with a space in between and a newline ("\n") at the end. Print the resulting string count times.
+str1 = input('Enter the first string: ')
+str2 = input('Enter the second string: ')
+count = int(input('Enter the count'))
+print('The first string entered was: ', str1, 'The second string entered was: ', str2, '\n And the count was: ', count )
