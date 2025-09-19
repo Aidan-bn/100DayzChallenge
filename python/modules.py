c13fa6140d620a46016a4d21eaa414b3200afbaa
Aidan <bantezea@gmail.com>
2025-09-19 14:32:51 +0300

    closing chapter seven

---
+++ python/modules.py
@@ -0,0 +1,13 @@
+import math
+def square(side):
+    return side**2
+
+def rectangle(length, width):
+    return length * width
+
+def triangle(base, height):
+    return 0.5 * base * height
+
+def trapeza(base1, base2, height):
+    return 0.5 * (base1 + base2) * height
+
